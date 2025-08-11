d02e039614356655137ccf0918a787a35586536c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-08-11 13:48:12 -0300

    Corrigindo erros

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 int main(){
-    printf("Hello World")
-    float numero = 10/0;
-    salkdjlasjdkasld
+    printf("Hello World");
+    float numero = 10/2;
+    printf(numero);
 }
